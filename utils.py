8bf37273eea838f6098465814fe547cbe11f61c4
Per Ivar Nerseth <perivar@nerseth.com>
2022-07-09 17:03:42 +0200

    first commit

---
+++ utils.py
@@ -0,0 +1,110 @@
+import cv2
+import numpy as np
+
+# most of the code is taken from here:
+# https://github.com/murtazahassan/Optical-Mark-Recognition-OPENCV/blob/master/utlis.py
+
+#### 1 - Preprocessing Image
+def preProcess(img):
+    imgGrayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
+    imgMaxContrastGrayscale = maximizeContrast(imgGrayscale)
+    imgBlur = cv2.GaussianBlur(imgMaxContrastGrayscale, (5, 5), 1) # ADD GAUSSIAN BLUR
+    
+    imgThreshold = cv2.adaptiveThreshold( 
+        imgBlur,
+        maxValue=255.0,
+        adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        thresholdType=cv2.THRESH_BINARY_INV,
+        blockSize=11,
+        C=2
+    )
+
+    return imgThreshold
+
+#### 1.1 - Preprocessing Image
+def maximizeContrast(imgGrayscale):
+    height, width = imgGrayscale.shape
+
+    imgTopHat = np.zeros((height, width, 1), np.uint8)
+    imgBlackHat = np.zeros((height, width, 1), np.uint8)
+
+    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    imgTopHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_TOPHAT, structuringElement)
+    imgBlackHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_BLACKHAT, structuringElement)
+
+    imgGrayscalePlusTopHat = cv2.add(imgGrayscale, imgTopHat)
+    imgGrayscalePlusTopHatMinusBlackHat = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+
+    return imgGrayscalePlusTopHatMinusBlackHat
+
+#### 2 - FINDING THE BIGGEST CONTOUR WITH OPTIONALLY 4 SIDES
+def biggestContour(contours, is4Sides = True):
+    biggest = np.array([])
+    max_area = 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 50:
+            if is4Sides:
+                peri = cv2.arcLength(cnt, True)
+                approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+                if area > max_area and len(approx) == 4:
+                    biggest = approx
+                    max_area = area                
+            else:
+                if area > max_area:
+                    biggest = cnt
+                    max_area = area
+
+    return biggest, max_area
+
+#### 3 - Reorder points for Warp Perspective, expects only 4 points
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+
+## 4 - TO STACK ALL THE IMAGES IN ONE WINDOW
+def stackImages(imgArray,scale,labels=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        # ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(labels) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        #print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(labels[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,labels[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+
